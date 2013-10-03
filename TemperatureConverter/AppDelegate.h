175553e55ca7e2bfe7f524a1a1a28fdea3d7914d
Ben Lindsey <ben.lindsey@gmail.com>
2013-10-02 23:05:31 -0700

    Initial Commit

---
+++ TemperatureConverter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TemperatureConverter
+//
+//  Created by Ben Lindsey on 10/2/13.
+//  Copyright (c) 2013 Ben Lindsey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
